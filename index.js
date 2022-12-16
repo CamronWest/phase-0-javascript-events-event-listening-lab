e0a9edfdc9d4915dbb3905bcdc508a2e96516224
Camron <Cawe219@gmail.com>
2022-12-16 00:00:25 -0600

    I fixed the send button but it doesn't send anything anywhere yet.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById('message');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    }); 
 }
+
+addingEventListener();
